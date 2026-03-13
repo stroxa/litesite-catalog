972830b4d6504de490ac4d6e5678ee3807193602
stroxa <153433394+stroxa@users.noreply.github.com>
2026-03-13 15:28:36 +0300

    updates-cache

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 const CACHE_NAME = "litesite-catalog-v1";
 
-const STATIC_ASSETS = [
+const CORE_ASSETS = [
   "./",
   "manifest.json",
   "s/css/catalog.css",
@@ -8,60 +8,109 @@ const STATIC_ASSETS = [
   "icon.svg",
   "icon-192.png",
   "icon-512.png",
-  "s/img/litesite-cover/litesite-brown-tr-index.webp",
-  "s/img/litesite-cover/litesite-blue-tr-index.webp",
-  "s/img/litesite-cover/litesite-red-tr-index.webp",
-  "s/img/litesite-cover/litesite-pink-tr-index.webp",
+  "s/img/litesite-cover/litesite-brown-tr-index.jpg",
+  "s/img/litesite-cover/litesite-blue-tr-index.jpg",
+  "s/img/litesite-cover/litesite-red-tr-index.jpg",
+  "s/img/litesite-cover/litesite-pink-tr-index.jpg",
 ];
 
-function cacheAssetsSafely() {
-  return caches.open(CACHE_NAME).then(function (cache) {
-    return Promise.all(
-      STATIC_ASSETS.map(function (url) {
-        return cache.add(url).catch(function () {});
+const DETAIL_ASSETS = [
+  "s/img/litesite-web/litesite-brown-tr-index.png",
+  "s/img/litesite-web/litesite-brown-tr-product.png",
+  "s/img/litesite-web/litesite-brown-en-index.png",
+  "s/img/litesite-web/litesite-brown-en-product.png",
+  "s/img/litesite-web/litesite-brown-ar-index.png",
+  "s/img/litesite-web/litesite-brown-ar-product.png",
+  "s/img/litesite-web/litesite-blue-tr-index.png",
+  "s/img/litesite-web/litesite-blue-tr-product.png",
+  "s/img/litesite-web/litesite-red-tr-index.png",
+  "s/img/litesite-web/litesite-red-tr-product.png",
+  "s/img/litesite-web/litesite-red-tr-hakkimizda.png",
+  "s/img/litesite-web/litesite-red-tr-menu.png",
+  "s/img/litesite-web/litesite-pink-tr-index.png",
+  "s/img/litesite-web/litesite-pink-tr-product.png",
+  "s/img/litesite-web/litesite-pink-tr-hakkimizda.png",
+  "s/img/litesite-web/litesite-pink-tr-menu.png",
+  "s/img/litesite-mobil/brown-tr-index.png",
+  "s/img/litesite-mobil/brown-tr-product.png",
+  "s/img/litesite-mobil/brown-en-index.png",
+  "s/img/litesite-mobil/brown-en-product.png",
+  "s/img/litesite-mobil/brown-ar-index.png",
+  "s/img/litesite-mobil/brown-ar-product.png",
+  "s/img/litesite-mobil/blue-tr-index.png",
+  "s/img/litesite-mobil/blue-tr-product.png",
+  "s/img/litesite-mobil/red-tr-index.png",
+  "s/img/litesite-mobil/red-tr-product.png",
+  "s/img/litesite-mobil/red-tr-hakkimizda.png",
+  "s/img/litesite-mobil/red-tr-menu.png",
+  "s/img/litesite-mobil/pink-tr-index.png",
+  "s/img/litesite-mobil/pink-tr-product.png",
+  "s/img/litesite-mobil/pink-tr-hakkimizda.png",
+  "s/img/litesite-mobil/pink-tr-menu.png",
+];
+
+const CORE_PATHS = CORE_ASSETS.map(function(url) {
+  return new URL(url, self.location).pathname;
+});
+
+self.addEventListener("install", function(e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function(c) { return c.addAll(CORE_ASSETS); })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(
+        keys.filter(function(k) { return k !== CACHE_NAME; })
+            .map(function(k) { return caches.delete(k); })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("message", function(e) {
+  if (e.data === "cache-all") {
+    caches.open(CACHE_NAME).then(function(c) {
+      Promise.allSettled(
+        DETAIL_ASSETS.map(function(url) { return c.add(url); })
+      );
+    });
+  }
+});
+
+self.addEventListener("fetch", function(e) {
+  if (e.request.method !== "GET") { return; }
+
+  const url = new URL(e.request.url);
+  const isCore = CORE_PATHS.indexOf(url.pathname) !== -1;
+
+  if (isCore) {
+    e.respondWith(
+      fetch(e.request).then(function(res) {
+        const clone = res.clone();
+        caches.open(CACHE_NAME).then(function(c) { c.put(url.pathname, clone); });
+        return res;
+      }).catch(function() {
+        return caches.match(url.pathname);
       })
     );
-  });
-}
+    return;
+  }
 
-function deleteOldCaches() {
-  return caches.keys().then(function (cacheNames) {
-    return Promise.all(
-      cacheNames
-        .filter(function (name) { return name !== CACHE_NAME; })
-        .map(function (name) { return caches.delete(name); })
-    );
-  });
-}
-
-function fetchAndCache(request) {
-  return fetch(request).then(function (networkResponse) {
-    return caches.open(CACHE_NAME).then(function (cache) {
-      cache.put(request, networkResponse.clone());
-      return networkResponse;
-    });
-  });
-}
-
-function serveRequest(request) {
-  return caches.match(request).then(function (cachedResponse) {
-    if (cachedResponse) { return cachedResponse; }
-    return fetchAndCache(request);
-  });
-}
-
-self.addEventListener("install", function (event) {
-  event.waitUntil(cacheAssetsSafely().then(function () {
-    return self.skipWaiting();
-  }));
-});
-
-self.addEventListener("activate", function (event) {
-  event.waitUntil(deleteOldCaches().then(function () {
-    return self.clients.claim();
-  }));
-});
-
-self.addEventListener("fetch", function (event) {
-  event.respondWith(serveRequest(event.request));
+  e.respondWith(
+    caches.match(e.request, { ignoreSearch: true }).then(function(cached) {
+      const fetched = fetch(e.request).then(function(res) {
+        const clone = res.clone();
+        caches.open(CACHE_NAME).then(function(c) { c.put(url.pathname, clone); });
+        return res;
+      }).catch(function() {
+        return cached;
+      });
+      return cached || fetched;
+    })
+  );
 });
